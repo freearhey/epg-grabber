c53ad84dd8139592c94068e004dc0b7a1b341063
freearhey <free.arhey@gmail.com>
2021-03-28 04:00:28 +0300

    Update utils.test.js

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -73,3 +73,10 @@ it('can escape string', () => {
   const string = 'Música тест dun.  &<>"\'\r\n'
   expect(utils.escapeString(string)).toBe('Música тест dun. &amp;&lt;&gt;&quot;&apos;')
 })
+
+it('can escape url', () => {
+  const string = 'http://example.com/logos/1TV.png?param1=val&param2=val'
+  expect(utils.escapeString(string)).toBe(
+    'http://example.com/logos/1TV.png?param1=val&amp;param2=val'
+  )
+})
