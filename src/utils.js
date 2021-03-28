ab027db13e833b733f19082081c3f0e64153fc25
freearhey <free.arhey@gmail.com>
2021-03-28 23:58:02 +0300

    Update utils.js
    Throws an error if a channel does not have a name

--- src/utils.js
+++ src/utils.js
@@ -60,6 +60,7 @@ utils.parseChannels = function (filename) {
     .filter(el => el.name === 'channel')
     .map(el => {
       const channel = el.attributes
+      if (!el.elements) throw new Error(`Channel '${channel.xmltv_id}' has no valid name`)
       channel.name = el.elements.find(el => el.type === 'text').text
       channel.site = channel.site || site.attributes.site
 
