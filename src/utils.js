97cee79609a16900ca3b01fcfb8007b862caf447
freearhey <free.arhey@gmail.com>
2021-04-02 20:30:33 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -41,8 +41,7 @@ utils.loadConfig = function (file) {
       method: 'GET',
       headers: {
         'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 Edg/79.0.309.71',
-        Cookie: ''
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 Edg/79.0.309.71'
       },
       timeout: 5000,
       withCredentials: true,
