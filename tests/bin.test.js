d20f0a75d84acad6dfb6af837501506318c0a5dd
Aleksandr Statciuk <free.arhey@gmail.com>
2021-10-06 00:39:01 +0300

    Create bin.test.js

---
+++ tests/bin.test.js
@@ -0,0 +1,37 @@
+const { execSync } = require('child_process')
+const pwd = `${__dirname}/..`
+
+function stdoutResultTester(stdout) {
+  return [`Finish`].every(val => {
+    return RegExp(val).test(stdout)
+  })
+}
+
+it('can load config', () => {
+  const result = execSync(
+    `node ${pwd}/bin/epg-grabber.js --config=tests/input/example.com.config.js`,
+    {
+      encoding: 'utf8'
+    }
+  )
+
+  expect(stdoutResultTester(result)).toBe(true)
+})
+
+it('can load mini config', () => {
+  const result = execSync(
+    `node ${pwd}/bin/epg-grabber.js \
+      --config=tests/input/mini.config.js \
+      --channels=tests/input/example.com.channels.xml \
+      --output=tests/output/mini.guide.xml \
+      --lang=fr \
+      --days=3 \
+      --delay=5000`,
+    {
+      encoding: 'utf8'
+    }
+  )
+
+  expect(stdoutResultTester(result)).toBe(true)
+  expect(result.includes("File 'tests/output/mini.guide.xml' successfully saved")).toBe(true)
+})
