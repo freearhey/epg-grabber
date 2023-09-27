8f000f81712e9bbc95a6bf52888520edec6b8a62
freearhey <7253922+freearhey@users.noreply.github.com>
2023-09-27 06:52:18 +0300

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -9,11 +9,19 @@ module.exports.parsePrograms = parsePrograms
 function parseChannels(xml) {
   const result = convert.xml2js(xml)
   const siteTag = result.elements.find(el => el.name === 'site') || {}
-  if (!siteTag.elements) return []
-  const rootSite = siteTag.attributes.site
 
-  const channelsTag = siteTag.elements.find(el => el.name === 'channels')
-  if (!channelsTag.elements) return []
+  const channelsTag =
+    siteTag && Array.isArray(siteTag.elements)
+      ? siteTag.elements.find(el => el.name === 'channels')
+      : result.elements.find(el => el.name === 'channels')
+  if (!channelsTag || !channelsTag.elements) return []
+
+  let rootSite = ''
+  if (siteTag && siteTag.attributes && siteTag.attributes.site) {
+    rootSite = siteTag.attributes.site
+  } else if (channelsTag && channelsTag.attributes && channelsTag.attributes.site) {
+    rootSite = channelsTag.attributes.site
+  }
 
   const channels = channelsTag.elements
     .filter(el => el.name === 'channel')
@@ -26,7 +34,7 @@ function parseChannels(xml) {
       return new Channel(c)
     })
 
-  return { site: rootSite, channels }
+  return channels
 }
 
 async function parsePrograms(data) {
