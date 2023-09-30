688310e4a91c43ce59c68037c63e2e93cef2d9be
freearhey <7253922+freearhey@users.noreply.github.com>
2023-09-30 05:29:42 +0300

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,7 @@
  * @jest-environment node
  */
 
-import { EPGGrabber, Channel } from '../src/index'
+import { EPGGrabber, EPGGrabberMock, Channel } from '../src/index'
 import axios from 'axios'
 
 jest.mock('axios')
@@ -63,6 +63,61 @@ it('can grab single channel programs', done => {
     })
     .then(programs => {
       expect(programs.length).toBe(0)
+      done()
+    })
+})
+
+it('can mock epg grabber', done => {
+  const config = {
+    site: 'example.com',
+    url: 'http://example.com/20210319/1tv.json',
+    parser: ({ channel, date }) => [
+      { title: `Test (${channel.name})`, start: '2021-03-19T04:30:00.000Z' }
+    ]
+  }
+  const channel = new Channel({
+    site: 'example.com',
+    site_id: '1',
+    xmltv_id: '1TV.fr',
+    lang: 'fr',
+    name: '1TV'
+  })
+  const grabber = new EPGGrabberMock(config)
+  grabber
+    .grab(channel, '2022-01-01', (data, err) => {
+      if (err) {
+        done()
+      }
+    })
+    .then(programs => {
+      expect(programs.length).toBe(1)
+      expect(programs).toEqual([
+        {
+          site: 'example.com',
+          channel: '1TV.fr',
+          titles: [{ value: 'Test (1TV)', lang: 'fr' }],
+          sub_titles: [],
+          descriptions: [],
+          icon: { src: '' },
+          episodeNumbers: [],
+          date: null,
+          start: 1616128200000,
+          stop: null,
+          urls: [],
+          ratings: [],
+          categories: [],
+          directors: [],
+          actors: [],
+          writers: [],
+          adapters: [],
+          producers: [],
+          composers: [],
+          editors: [],
+          presenters: [],
+          commentators: [],
+          guests: []
+        }
+      ])
       done()
     })
 })
