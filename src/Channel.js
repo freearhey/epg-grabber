60d5df6a7ded4c86bdfe5f3e010260568c298aab
freearhey <7253922+freearhey@users.noreply.github.com>
2024-06-26 13:59:37 +0300

    Update Channel.js

--- src/Channel.js
+++ src/Channel.js
@@ -1,13 +1,18 @@
+const { toArray } = require('./utils')
+
 class Channel {
   constructor(c) {
     const data = {
       xmltv_id: c.xmltv_id,
-      name: c.name,
+      displayNames: toArray(c.displayNames || c.displayName || c.names || c.name).map(text =>
+        toTextObject(text, c.lang)
+      ),
       site: c.site || '',
       site_id: c.site_id,
       lang: c.lang || '',
-      logo: c.logo || '',
-      url: c.url || toURL(c.site)
+      icons: toArray(c.icons || c.icon || c.logo).map(toIconObject),
+      urls: toArray(c.urls || c.url || toURL(c.site)).map(toUrlObject),
+      lcn: toArray(c.lcn).map(toTextObject)
     }
 
     for (let key in data) {
@@ -20,4 +25,35 @@ module.exports = Channel
 
 function toURL(site) {
   return site ? `https://${site}` : ''
+}
+
+function toTextObject(text, lang) {
+  if (typeof text === 'string') {
+    return { value: text, lang }
+  }
+
+  return {
+    value: text.value,
+    lang: text.lang
+  }
+}
+
+function toIconObject(icon) {
+  if (!icon) return { src: '' }
+  if (typeof icon === 'string') return { src: icon }
+
+  return {
+    src: icon.src,
+    width: icon.width,
+    height: icon.height
+  }
+}
+
+function toUrlObject(url) {
+  if (typeof url === 'string') return { system: '', value: url }
+
+  return {
+    system: url.system || '',
+    value: url.value || ''
+  }
 }
