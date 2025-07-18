e7142572d81f9aa262bd1c3c0bc564c1ee0fc0ec
freearhey <7253922+freearhey@users.noreply.github.com>
2025-07-18 22:07:32 +0300

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,35 +7,6 @@ import axios from 'axios'
 
 jest.mock('axios')
 
-it('return "Connection timeout" error if server does not response', done => {
-  const config = {
-    site: 'example.com',
-    request: {
-      timeout: 1000
-    },
-    url({ date, channel }) {
-      return `https://www.cosmote.gr/cosmotetv/residential/program/epg/programchannel?p_p_id=channelprogram_WAR_OTETVportlet&p_p_lifecycle=0&_channelprogram_WAR_OTETVportlet_platform=IPTV&_channelprogram_WAR_OTETVportlet_date=${date.format(
-        'DD-MM-YYYY'
-      )}&_channelprogram_WAR_OTETVportlet_articleTitleUrl=${channel.site_id}`
-    },
-    parser: () => []
-  }
-  const channel = new Channel({
-    site: 'example.com',
-    site_id: 'cnn',
-    xmltv_id: 'CNN.us',
-    lang: 'en',
-    name: 'CNN'
-  })
-  const grabber = new EPGGrabber(config)
-  grabber
-    .grab(channel, '2022-01-01', (data, err) => {
-      expect(err.message).toBe('Connection timeout')
-      done()
-    })
-    .catch(done)
-})
-
 it('can grab single channel programs', done => {
   const data = {
     data: {
