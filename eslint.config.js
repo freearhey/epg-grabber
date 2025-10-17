1b63b2eceb76ded066657ddd556998209df328f6
freearhey <7253922+freearhey@users.noreply.github.com>
2025-10-17 05:52:53 +0300

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended)
